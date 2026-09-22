2b0e78d0e02831defa978ca8cafbe6d571dc59b3
dezlymacauley <dezlymacauley@gmail.com>
2026-09-22 13:36:04 +0200

    Added guide for arrays of JSON

--- d03-external-dependencies/d01-serde-and-serde-json/src/bin/f03_arrays_of_json.rs
+++ d03-external-dependencies/d01-serde-and-serde-json/src/bin/f03_arrays_of_json.rs
@@ -11,9 +11,51 @@ fn main() {
 
     // EXAMPLE: 1 => How to create the array of JSON
 
-    let list_of_orders: Value = json!([]);
+    let mut list_of_orders: Vec<Value> = vec![];
+    //_________________________________________________________________________
 
-    println!("\nlist_of_orders");
-    println!("{list_of_orders}\n");
+    // EXAMPLE: 2 => How to add elements to the JSON array
+
+    list_of_orders.push(json!(
+        {
+          "orderId": "44873812-9570-48d8-bff7-afa105b15e57",
+          "status": "outForDelivery",
+          "isPickup": false,
+          "customer": {
+            "name": "Sarah Jenkins",
+            "address": "123 Main St, Apt 4B"
+          },
+          "driver": {
+            "name": "Marcus",
+            "phone": "555-0198",
+            "vehicle": "Silver Toyota Corolla"
+          },
+          "items": ["Pepperoni Pizza", "Spicy nuggets"],
+          "discount": null,
+          "totalPrice": 24.50
+        }
+    ));
+
+    list_of_orders.push(json!(
+        {
+          "orderId": "91b2e67a-1120-438c-8ef2-2a91176b91c4",
+          "status": "preparing",
+          "isPickup": true,
+          "customer": {
+            "name": "Alex Chen",
+            "address": "456 Oak Ave, Suite 101"
+          },
+          "driver": null,
+          "items": ["Veggie Burger", "Sweet Potato Fries", "Iced Tea"],
+          "discount": 3.50,
+          "totalPrice": 16.20
+        }
+    ));
+
+    println!("Raw payload\n");
+    println!("{}\n", json!(list_of_orders));
+    
+    println!("Pretty Print\n");
+    println!("{:#}\n", json!(list_of_orders));
     //_________________________________________________________________________
 }
